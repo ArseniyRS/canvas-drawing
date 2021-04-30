325b3b4e69930d6f4511160e33b8a07c750fa016
Arseniy <mrsinkrait@gmail.com>
2021-04-30 15:29:54 +0600

    added line,rect,circle,eraser

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -4,15 +4,19 @@ import {brushSVG, circleSVG, eraserSVG, lineSVG, rectSVG, redoSVG, saveSVG, undo
 import toolState from "../store/toolState";
 import canvasState from "../store/canvasState";
 import Brush from "../tools/Brush";
+import Eraser from "../tools/Eraser";
+import Line from "../tools/Line";
+import Circle from "../tools/Circle";
+import Rect from "../tools/Rect";
 const Toolbar = () => {
     return (
         <div className={'toolbar'}>
             <div className="toolbar__btn-container">
             <button className={'toolbar__btn'} onClick={()=>toolState.setTool((new Brush(canvasState.canvas)))}><img src={brushSVG} alt=""/></button>
-            <button className={'toolbar__btn'}><img src={rectSVG} alt=""/></button>
-            <button className={'toolbar__btn'}><img src={circleSVG} alt=""/></button>
-            <button className={'toolbar__btn'}><img src={eraserSVG} alt=""/></button>
-            <button className={'toolbar__btn'}><img src={lineSVG} alt=""/></button>
+            <button className={'toolbar__btn'} onClick={()=>toolState.setTool((new Rect(canvasState.canvas)))}><img src={rectSVG} alt=""/></button>
+            <button className={'toolbar__btn'} onClick={()=>toolState.setTool((new Circle(canvasState.canvas)))}><img src={circleSVG} alt=""/></button>
+            <button className={'toolbar__btn'} onClick={()=>toolState.setTool((new Eraser(canvasState.canvas)))}><img src={eraserSVG} alt=""/></button>
+            <button className={'toolbar__btn'} onClick={()=>toolState.setTool((new Line(canvasState.canvas)))}><img src={lineSVG} alt=""/></button>
                 <input type="color"/>
             </div>
             <div className="toolbar__btn-container">
