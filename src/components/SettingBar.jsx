77aa9658c8b01b3bacbbbfa53638da662f901701
Arseniy <mrsinkrait@gmail.com>
2021-05-27 15:17:28 +0600

    work on settingbar and toolstate

--- src/components/SettingBar.jsx
+++ src/components/SettingBar.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import toolState from "../store/toolState";
 
 const SettingBar = () => {
     return (
         <div className={'toolbar'} style={{top:40}}>
-            
+            <label htmlFor="line-width">Толщина линии</label>
+            <input 
+                onChange={e=>toolState.setLineWidth(e.target.value)}
+                type="number" 
+                defaultValue={1} 
+                min={1} 
+                max={50}/>
+            <label htmlFor="stroke-color">Цвет обводки</label>
+            <input
+                onChange={e=>toolState.setStrokeColor(e.target.value)}
+                type="color"
+                id={'stroke-color'}/>
         </div>
     );
 };
